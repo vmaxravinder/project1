a9ae468f4cfe25f632574adf096374adfe5ef522
Ravindar Reddy Yampalla <vmax.ravinder@gmail.com>
2018-03-07 09:50:10 +0530

    test.php has been modifief with little changes

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
-zxvzxdvzxvfgjfgjfg fgjfgjfgj
+
 
 git remote set-url origin ssh://git@github.com/username/testrepository.git
 
-zxvzxdvzxvfgjfgjfg fgjfgjfgj
 
 git remote add origin ssh://git@github.com/username/testrepository.gitdfbhgfhdfhdfh
